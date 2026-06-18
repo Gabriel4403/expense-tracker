172eecd444af018cc20da490fac4c0fb5635c01a
Onofrei Gabriel <onofreigabriel86@gmail.com>
2026-06-18 12:29:15 +0300

    mobile view fix

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -26,7 +26,7 @@ function Expenses({ items, onDeleteExpense, onEditExpense }) {
   const sortedExpenses = filteredExpenses.sort((a, b) => b.date - a.date);
 
   return (
-    <Card className="p-4 bg-[#181C14] mt-16 sm:mt-45 mx-auto w-[50rem] max-w-[95%] rounded-2xl shadow-xl">
+    <Card className="p-4 bg-[#181C14] mt-28 sm:mt-45 mx-auto w-[50rem] max-w-[92%] rounded-2xl shadow-xl">
       <ExpensesCtgFilter
         selected={filteredCategory}
         onChangeCategory={setFilteredCategory}
